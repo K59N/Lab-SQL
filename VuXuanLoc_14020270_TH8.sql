e06059c0ad41e0430343552d093ba096796ddf8b
Loc VU <locvx1234@gmail.com>
2015-11-08 01:34:18 +0700

    Complete

---
+++ VuXuanLoc_14020270_TH8.sql
@@ -0,0 +1,45 @@
+-- Creat by Loc VU
+-- Date 4/11/2015
+
+use classicmodels
+
+SELECT productCode, productName FROM products
+WHERE productCode in 
+(SELECT productCode FROM orderdetails WHERE orderNumber in 
+	(SELECT orderNumber FROM orders 
+		WHERE EXTRACT(YEAR FROM orderDate) = 2005 and EXTRACT(MONTH FROM orderDate) = 3));
+
+SELECT DISTINCT P.productCode, P.productName FROM products P
+INNER JOIN orderdetails OD ON OD.productCode = P.productCode
+INNER JOIN orders O ON O.orderNumber =  OD.orderNumber
+WHERE EXTRACT(YEAR FROM orderDate) = 2005 and EXTRACT(MONTH FROM orderDate) = 3;
+
+
+-- SELECT * FROM orders WHERE EXTRACT(YEAR FROM orderDate) = (SELECT MAX(EXTRACT(YEAR FROM orderDate)) FROM orders) and 
+-- EXTRACT(MONTH FROM orderDate) = (SELECT MAX(EXTRACT(MONTH FROM orderDate)) FROM orders 
+-- 	WHERE EXTRACT(YEAR FROM orderDate) = (SELECT MAX(EXTRACT(YEAR FROM orderDate)) FROM orders)); 
+SELECT * FROM orders WHERE EXTRACT(YEAR FROM orderDate) = EXTRACT(YEAR FROM (SELECT MAX(orderDate) FROM orders)) and
+EXTRACT(MONTH FROM orderDate) = EXTRACT(MONTH FROM (SELECT MAX(orderDate) FROM orders))
+
+SELECT orderNumber, orderDate, requiredDate, status, total
+FROM orders , (SELECT orderNumber, sum(orderdetails.priceEach*orderdetails.quantityOrdered) as total FROM orderdetails GROUP BY orderNumber) AS TT 
+WHERE orders.orderNumber = TT.orderNumber;
+
+SELECT orderNumber, orderDate, (SELECT SUM(quantityOrdered*priceEach) FROM orderdetails WHERE orderNumber = O.orderNumber) AS TotalCost
+FROM orders O;
+
+
+
+SELECT C.customerNumber, C.customerName, totalOrder, totalPayment
+FROM 
+	customers C, 
+	
+	(SELECT customerNumber , SUM(amount) as totalPayment 
+	FROM payments 
+	GROUP BY customerNumber) as Sum_amount, 
+
+	(SELECT  OD.orderNumber, SUM(quantityOrdered*priceEach) as totalOrder, customerNumber 
+	FROM orderdetails OD, orders O 
+	WHERE OD.orderNumber = O.orderNumber 
+	GROUP BY customerNumber) as  Sum_order 
+WHERE C.customerNumber = Sum_amount.customerNumber AND C.customerNumber = Sum_order.customerNumber;
